d974623137a6c1b39689baf6fd52d254c873db19
Zdenek Hynek <zdenek.hynek@gmail.com>
2014-02-15 14:55:29 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,124 @@
+var map;
+
+function initialize() {
+
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(51.515000000000001, -0.15500000000000003)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+var foursquareApiId = "21M5CXSCBBOYRITEKRXDR2KHHNHHK1UVT0FX1CQUJKLLHD23";
+var foursquareSecret = "YUYZUCL13PS0KUQUMWIHKBBAOGE0VG4NMW0S0TPA0C0XPWOS";
+
+//https://api.foursquare.com/v2/venues/search?client_id=21M5CXSCBBOYRITEKRXDR2KHHNHHK1UVT0FX1CQUJKLLHD23&client_secret=YUYZUCL13PS0KUQUMWIHKBBAOGE0VG4NMW0S0TPA0C0XPWOS&v=20130815&ll=40.7,-74&query=sushi
+
+//https://api.foursquare.com/v2/venues/search?client_id=21M5CXSCBBOYRITEKRXDR2KHHNHHK1UVT0FX1CQUJKLLHD23&client_secret=YUYZUCL13PS0KUQUMWIHKBBAOGE0VG4NMW0S0TPA0C0XPWOS&v=20130815&ll=40.7,-74
+
+
+function getPlaces( lat, lng ) {
+
+	console.log( "getPlaces", lat, lng );
+
+	var url = "https://api.foursquare.com/v2/venues/search?client_id=" + foursquareApiId + "&client_secret=" + foursquareSecret + "&v=20130815&ll=" + lat + "," + lng;
+
+	console.log( "url", url );
+
+	$.ajax( {
+
+		url: url,
+		success: function( data ) {
+
+			console.log( "success" );
+			console.log( data );
+
+		}, 
+		error: function( xhr ) {
+
+			console.error( "error request" );
+			console.error( xhr );
+
+
+		}
+
+
+	});
+
+
+}
+
+
+getPlaces( 40.7,-74 );
+
+
+function addMarker( lat, lng ) {
+
+	var marker = new google.maps.Marker( {
+		position: new google.maps.LatLng( lat, lng ),
+		map: map
+	});
+
+}
+
+var north = 51.539289;
+var south = 51.478069;
+var west = -0.192803;
+var east = -0.069721;
+var step = 0.005;
+
+var nowLng = west, nowLat = north;
+var numberOfPoints = 0;
+var limit = 20;
+
+var interval = setInterval( function() {
+
+	// w->e, n->s
+	if( numberOfPoints >= limit ) {
+		console.log( "limit!!!");
+		return;
+	}
+
+	if( nowLng < east ) {
+		nowLng += step; 
+	} else {
+
+		//skip to next 
+		nowLng = west;
+
+		if( nowLat > south ) {
+
+			nowLat -= step;
+
+		} else {
+
+			//all done
+			end();
+			return;
+
+		}
+
+
+	}
+
+	console.log( nowLat, nowLng, numberOfPoint );
+	addMarker( nowLat, nowLng );
+
+	numberOfPoints++;
+
+}, 10 );
+
+
+function end() {
+
+	console.log( "end" );
+
+	clearInterval( interval );
+
+}
+
+
