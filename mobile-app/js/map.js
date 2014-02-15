cb56cc7e957b1281db82f3efc307573e78aec8b1
Zdenek Hynek <zdenek.hynek@gmail.com>
2014-02-15 16:35:31 +0000

    mobile app

---
+++ mobile-app/js/map.js
@@ -0,0 +1,18 @@
+var map = L.map('map');
+map.locate({setView: true, maxZoom: 16});
+
+L.tileLayer('http://{s}.tile.cloudmade.com/API-key/997/256/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    maxZoom: 18
+}).addTo(map);
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
