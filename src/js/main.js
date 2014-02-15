65ee2f6d8d8c134ce10e454e3005e73f153a605e
Mark Durrant <mark@pebblecode.com>
2014-02-15 22:40:08 +0000

    add header & responsive shiz

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 $( document ).ready( function() {
 
+  // Go back to main when clicking header
+  $( 'header' ).click( function( event ) {
+    $( '.main, header' ).toggleClass( 'active' );
+  });
+
 });
